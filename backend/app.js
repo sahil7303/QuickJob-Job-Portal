e7dbf7f5ec0a17ae6d8a0b951c730c900d692579
sahil7303 <sahildewangan73@gmail.com>
2025-01-29 23:44:56 +0530

    connected frontend with backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
-import jobRouter from "./routes/jobRoutes.js";
+import jobRouter from "./routes/jobRoutes.js";path
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
 import { config } from "dotenv";
@@ -8,6 +8,7 @@ import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import path from "path";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -27,9 +28,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send(`Server is running on PORT: ${process.env.PORT}`);
-});
+// app.get("/", (req, res) => {
+//   res.send(`Server is running on PORT: ${process.env.PORT}`);
+// });
 
 app.use(
   fileUpload({
@@ -41,6 +42,13 @@ app.use(
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res)=>{
+  res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
+});
 
 dbConnection();
 
