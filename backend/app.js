b7d29ff85f4f8cf2e0b6ea94bc65d56ecc971678
sahil7303 <sahildewangan73@gmail.com>
2025-01-30 01:08:47 +0530

    app path resolved

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res)=>{
-  res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 });
 
 dbConnection();
